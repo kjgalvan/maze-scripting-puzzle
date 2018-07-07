e94e8038f76ff578965e573746488d7fbdb738dd
root <kaityagalvan@gmail.com>
2018-07-07 13:52:50 -0700

    Finished solver with recursion

--- solution/solution.py
+++ solution/solution.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-def neighbors(row, col):
+def getNeighbors(row, col):
     neighbors = []
-    neighbors.append([row+1, col])
-    neighbors.append([row-1, col])
-    neighbors.append([row, col+1])
-    neighbors.append([row, col-1])
+    neighbors.append((row+1, col))
+    neighbors.append((row-1, col))
+    neighbors.append((row, col+1))
+    neighbors.append((row, col-1))
     return neighbors
 
 def direction(firstRow, firstCol, secondRow, secondCol):
@@ -32,22 +32,27 @@ end = "-"
 pathIndexes = []
 
 for index, value in enumerate(board):
-    if path in value:
-        subindex = value.index(path)
-        pathIndexes.append([index, subindex])
+    for subIndex, subValue in enumerate(value):
+        if path == subValue:
+            pathIndexes.append((index, subIndex))
     if start in value:
         subindex = value.index(start)
-        startIndex = [index, subindex]
+        startIndex = (index, subindex)
     if end in value:
         subindex = value.index(end)
-        endIndex = [index, subindex]
+        endIndex = (index, subindex)
 
-neighbors = neighbors(*startIndex)
+def directionLoop(startIndex, endIndex, pathIndexes, directionString):
+    if (endIndex == startIndex):
+        return directionString
+    else:
+        neighbors = getNeighbors(*startIndex)
+        nextStepSet = set(neighbors).intersection(pathIndexes)
+        nextStep = list(nextStepSet)[0]
+        newPath = [x for x in pathIndexes if x != nextStep]
+        newStartIndex = nextStep
+        newDirString = directionString + direction(startIndex[0], startIndex[1], nextStep[0], nextStep[1])
+        return directionLoop(newStartIndex, endIndex, newPath, newDirString)
 
-neighborsTuple = tuple(tuple(x) for x in neighbors)
-pathTuple = tuple(tuple(x) for x in pathIndexes)
-
-nextStepSet = set(neighborsTuple).intersection(pathTuple)
-nextStep = list(list(nextStepSet)[0])
-print nextStep
-print (direction(startIndex[0], startIndex[1], nextStep[0], nextStep[1]))
+pathIndexes.append(endIndex)
+print directionLoop(startIndex, endIndex, pathIndexes, "")
